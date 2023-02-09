53a722ec4eb185f9f4fe2a004dcd621b3ab34aeb
Raul Molina <rmolina2003.rm@gmail.com>
2023-02-09 16:13:55 +0100

    example

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,16 @@
 <?php
 
 use App\Http\Controllers\AppController;
+use App\Http\Controllers\userController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [AppController::class,'index']);
 Route::get('/form', [AppController::class,'procesar']);
 Route::get('/productos', [AppController::class,'verProductos']);
 Route::get('/busca', [AppController::class,'busca']);
+
+Route::get('/loginForm', [userController::class,function () {
+    return view('loginForm');
+}]);
+Route::post('/login', [userController::class,'login']);
+Route::get('/logout', [userController::class,'logout']);
