3c89ea49517d33436bd40af9d721ad52bba19143
Raul-Molinaaguilar_tvlport <21rmolina@ibadia.cat>
2023-03-09 11:59:59 +0100

    dashboard update

--- resources/views/home.blade.php
+++
@@ -1,8 +0,0 @@
-<link rel="stylesheet" href="https://unpkg.com/@picocss/pico@1.*/css/pico.min.css">
-@if (Session::get('user'))
-    <h1>bienvenido ususario {{ Session::get('user') }}</h1>
-    <p><a href="{{ url('/logout') }}">Salir</a></p>
-@else
-    <h1>este usuario no existe</h1>
-    <p><a href="{{ url('/loginForm') }}">Iniciar sesión</a></p>
-@endif
--- resources/views/productos.blade.php
+++
@@ -1,7 +0,0 @@
-<link rel="stylesheet" href="https://unpkg.com/@picocss/pico@1.*/css/pico.min.css">
-@foreach ($productos as $producto)
-    Nombre: {{ $producto->nombre }} <br>
-    Descripcion: {{ $producto->descripcion }} <br>
-    Precio: {{ $producto->precio }} <br>
-    Imagen: {{ $producto->imagen }}
-@endforeach
